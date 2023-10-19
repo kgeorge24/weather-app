75cc15294dba481e93a776a7598fe2e75f0bd5b2
Kyle George <kylegeorge@Kyles-MBP.lan>
2023-10-19 15:57:42 -0400

    Connected search component to the new API

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,23 +1,7 @@
-import SearchResults from "./SearchResults";
 import searchIcon from "../../assets/search.png";
 import styles from "./Search.module.css";
 
 const Search = props => {
-  // Returns list of cities that match users input
-  const searchResults = () => {
-    return props.searchResults.length > 0
-      ? props.searchResults.map((result) => {
-          return (
-            <SearchResults
-              result={result}
-              key={result.lat + Math.random()}
-              getWeatherResults={props.getWeatherResults}
-            />
-          );
-        })
-      : null;
-  };
-
   return (
     <div className={styles["search-container"]}>
       <form onSubmit={props.searchFormSubmitHandler}>
@@ -33,7 +17,6 @@ const Search = props => {
           <img src={searchIcon} alt="search icon" />
         </button>
       </form>
-      <ul>{props.showResults ? searchResults() : null}</ul>
     </div>
   );
 };
